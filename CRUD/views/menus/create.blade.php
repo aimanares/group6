57ba741aabbbacbc90d8f58401398ec699e4dcda
nurasimah181 <55780049+nurasimah181@users.noreply.github.com>
2021-06-13 16:28:25 +0800

    add CRUD

---
+++ CRUD/views/menus/create.blade.php
@@ -0,0 +1,47 @@
+@extends('menus.layout')
+  
+@section('content')
+<div class="row">
+    <div class="col-lg-12 margin-tb">
+        <div class="pull-left">
+            <h2>Create New Menu</h2>
+        </div>
+        <div class="pull-right">
+            <a class="btn btn-primary" href="{{ route('menus.index') }}"> Back</a>
+        </div>
+    </div>
+</div>
+   
+@if ($errors->any())
+    <div class="alert alert-danger">
+        <strong>Warning!</strong> Please check your input code<br><br>
+        <ul>
+            @foreach ($errors->all() as $error)
+                <li>{{ $error }}</li>
+            @endforeach
+        </ul>
+    </div>
+@endif
+   
+<form action="{{ route('menus.store') }}" method="POST">
+    @csrf
+     <div class="row">
+        <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="form-group">
+                <strong>Menu Name:</strong>
+                <input type="text" name="menuName" class="form-control" placeholder="menuName">
+            </div>
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="form-group">
+                <strong>Price:</strong>
+                <input type="text" name="price" class="form-control" placeholder="price">
+            </div>
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-12 text-center">
+                <button type="submit" class="btn btn-primary">Submit</button>
+        </div>
+    </div>
+   
+</form>
+@endsection
---
+++ CRUD/views/menus/show.blade.php
@@ -0,0 +1,28 @@
+@extends('menus.layout')
+@section('content')
+    <div class="row">
+        <div class="col-lg-12 margin-tb">
+            <div class="pull-left">
+                <h2> Show Menu</h2>
+            </div>
+            <div class="pull-right">
+                <a class="btn btn-primary" href="{{ route('menus.index') }}"> Back</a>
+            </div>
+        </div>
+    </div>
+   
+    <div class="row">
+        <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="form-group">
+                <strong>Menu Name:</strong>
+                {{ $menu->menuName }}
+            </div>
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="form-group">
+                <strong>Price:</strong>
+                {{ $Menu->price }}
+            </div>
+        </div>
+    </div>
+@endsection
