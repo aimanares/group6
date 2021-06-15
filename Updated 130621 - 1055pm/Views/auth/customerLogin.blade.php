a5327e0a822c890eba87943616d0fde19f9bd7ee
aimanares <61780861+aimanares@users.noreply.github.com>
2021-06-15 08:23:18 +0800

    Update customerLogin.blade.php

--- Updated 130621 - 1055pm/Views/auth/customerLogin.blade.php
+++ Updated 130621 - 1055pm/Views/auth/customerLogin.blade.php
@@ -5,6 +5,15 @@
     <div class="container">
         <div class="row justify-content-center">
             <div class="col-md-8">
+            @if ($errors->any())
+                <div class="alert alert-danger">
+                    <ul>
+                        @foreach ($errors->all() as $error)
+                            <li>{{ $error }}</li>
+                        @endforeach
+                    </ul>
+                </div>
+            @endif
                 <div class="card">
                     <div class="card-header">{{ __('Customer Login') }}</div>
                     <div class="card-body">
