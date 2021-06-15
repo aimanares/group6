708b5f5c9b3806fcace29122ad91b10df8a8bb34
nazmi038 <61735034+nazmi038@users.noreply.github.com>
2021-06-15 11:57:27 +0800

    Add files via upload

---
+++ views/nav.blade.php
@@ -0,0 +1,34 @@
+<!--nav-bar-->
+<div align="center">
+<table width=13% border="10"  bordercolor="#0066FF">
+<tr style="background: #0066FF">
+<td colspan="3"><div  align="center"><h3>Links</h3></div></td>
+</tr>
+<tr style="background: #0066FF">
+<td>
+ <div align="center"><a href="/team"><FORM NAME="Form1">
+   <SCRIPT FOR="Button1" EVENT="onClick" LANGUAGE="VBScript">
+      MsgBox "Button Pressed!"
+   </SCRIPT>
+   <input type="Button" name="Button2" value="Buy" />
+ </FORM></a>&nbsp;</div></td>
+  <td>
+<div align="center"><a href="/services"><FORM NAME="Form1">
+   <INPUT TYPE="Button" NAME="Button1" VALUE="Details">
+   <SCRIPT FOR="Button1" EVENT="onClick" LANGUAGE="VBScript">
+      MsgBox "Button Pressed!"
+   </SCRIPT>
+   
+</FORM></a></div></td>
+<td>
+<div align="center"><a href="/about"><FORM NAME="Form1">
+   <INPUT TYPE="Button" NAME="Button3" VALUE="Home">
+   <SCRIPT FOR="Button1" EVENT="onClick" LANGUAGE="VBScript">
+      MsgBox "Button Pressed!"
+   </SCRIPT>
+   
+</FORM></a></div></td>
+</tr>
+
+</table>
+</div>
