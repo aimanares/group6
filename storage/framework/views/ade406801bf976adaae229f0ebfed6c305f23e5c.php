67ea5adf3bfb5fe59081f9bce7d0a0f42d593deb
nazmi038 <61735034+nazmi038@users.noreply.github.com>
2021-06-15 09:20:51 +0800

    Add files via upload

---
+++ storage/framework/views/ade406801bf976adaae229f0ebfed6c305f23e5c.php
@@ -0,0 +1,62 @@
+
+<?php $__env->startSection('title', 'Services'); ?>
+<?php $__env->startSection('content'); ?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<link rel="shortcut icon" href="http://www.supermap.com/favicon.ico" type="image/x-icon">
+
+<title>Group names</title>
+
+
+</head>
+
+<body>
+<audio controls autoplay>
+ 
+  <source src="Gambar jam/01 - Ichiban no Takaramono -Yui final ver.-.mp3" type="audio/mpeg">
+  Your browser does not support the audio element.
+</audio>
+<div align="center">
+<table width=50% border="1">
+
+<tr style="background: #938AF4">
+<td colspan="2" ><div align="center" class="style1"><h2 class="style4"><u>Contacts </u></h2>
+</div></td>
+</tr>
+
+<tr>
+<td style="background: #938AF4"><div align="center">   1.</div></td>
+<td style="background: #938AF4"><div align="center">   MOHAMMAD AIMAN BIN MOHAMAD ARES (1817363).</div></td>
+</tr>
+<tr>
+<td style="background: #938AF4"><div align="center">   2.</div></td>
+<td style="background: #938AF4"><div align="center">   NUR ASIMAH BINTI CHE MAMAT (1813212)</div></td>
+</tr>
+<tr>
+<td style="background: #938AF4"><div align="center">   3.</div></td>
+<td style="background: #938AF4"><div align="center">   MOHAMAD NAZMI BIN NAZARUDIN (1811239)</div></td>
+</tr>
+<tr>
+<td style="background: #938AF4"><div align="center">   4.</div></td>
+<td style="background: #938AF4"><div align="center">  WAN AHMAD LUQMAN AL - HAKIM WAN AZMI (1814213)</div></td>
+</tr>
+
+
+
+</table>
+<br />
+<br />
+<br />
+
+
+
+   <br />
+
+ 
+
+</body>
+<body background="Gambar jam/kanade-tenshi-our-angel-beats-family-30808404-1600-900.jpg" >
+</html>
+
+<?php echo $__env->make('app', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH C:\Users\PCNazmi\FoodOrder\resources\views/services.blade.php ENDPATH**/ ?>
