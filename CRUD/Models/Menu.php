ab9ff83bab84c40067eeb0b34d43e735d9633a51
nurasimah181 <55780049+nurasimah181@users.noreply.github.com>
2021-06-17 16:48:41 +0800

    Update Menu.php

--- CRUD/Models/Menu.php
+++ CRUD/Models/Menu.php
@@ -8,6 +8,9 @@ use Illuminate\Database\Eloquent\Model;
 class Menu extends Model
 {
     protected $fillable = [
-        'menuName', 'price'
-      ];
+        'menuName', 
+        'price',
+        'availability',
+        'cafe_id',
+    ];
 }
